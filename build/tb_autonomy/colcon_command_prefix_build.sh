76f058cc9bd222cc0da723c3aee9484da4e7d59f
Jing Huang <jinghuang809@gmail.com>
2026-02-25 22:43:49 -0500

    got it running

---
+++ build/tb_autonomy/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/dmin/turtlebot_ws/install/tb_worlds/share/tb_worlds/package.sh"
---
+++ build/tb_worlds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dmin/turtlebot_ws/src/turtlebot3_behavior_demos/tb_worlds
+# Build directory: /home/dmin/turtlebot_ws/build/tb_worlds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
